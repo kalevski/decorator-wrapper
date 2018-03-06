a9c4818c117bd70220f1738e0de5cde7a8984932
Pavle Jonoski <jonoski.pavle@gmail.com>
2018-03-07 00:48:26 +0100

    Setting the prototype of the class so 'instanceof' would recognize the created objects correctly.

--- src/classDecoration.js
+++ src/classDecoration.js
@@ -1,11 +1,14 @@
 export default (target, key, descriptor, decoratorInstance, decoratorArgs) => {
     let createInstance = (...args) => new target(...args);
     decoratorInstance.classDefined(target.name, createInstance, decoratorArgs);
-    return (...args) => {
+    let wrapped_type = (...args) => {
         let instance = decoratorInstance.classInstanced(target.name, args, createInstance, decoratorArgs);
         if (typeof instance === 'undefined') {
             instance = new target(...args);
         }
         return instance;
     };
+    
+    wrapped_type.prototype = target.prototype; // making sure 'instanceof' recognizes the object type
+    return wrapped_type;
 }
